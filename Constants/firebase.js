928ef5a50d946a693ed4eff4b2913a41054e60a7
Giuliano <giulianoperrinidev@gmail.com>
2022-06-16 16:15:42 -0300

    tarea

---
+++ Constants/firebase.js
@@ -0,0 +1,5 @@
+export const DB_URL = "https://curso-coder-default-rtdb.firebaseio.com/";
+export const AUTH_SIGNUP =
+  "https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyAkPuiIG5eSYWKm2CnvYagwjbZsB4-KesU";
+export const AUTH_LOGIN =
+  "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyAkPuiIG5eSYWKm2CnvYagwjbZsB4-KesU";
